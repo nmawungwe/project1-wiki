6ef848f16b2b3c974512e331ff35ee9ae889ac8f
Nyasha Vision <nmawungwe@gmail.com>
2020-10-23 21:45:59 +0200

    homepage search function partially working

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -2,6 +2,10 @@ from django.urls import path
 
 from . import views
 
+
+
+
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    # path("wiki/<str:title>", views.query, name="query")
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,10 +1,49 @@
 from django.shortcuts import render
+from django import forms
 
 from . import util
+from markdown2 import Markdown
 
+markdowner = Markdown()
+
+class Search(forms.Form):
+    title = forms.CharField(widget=forms.TextInput(attrs={'class': 'search', 'placeholder':'Search'}))
+
+# def index(request):
+#     return render(request, "encyclopedia/index.html", {
+#         "entries": util.list_entries()
+#     })
 
 def index(request):
-    return render(request, "encyclopedia/index.html", {
-        "entries": util.list_entries()
-    })
+    query = []
+    queries = util.list_entries()
+    if request.method =='POST':
+        form = Search(request.POST)
+        if form.is_valid():
+            title = form.cleaned_data["title"]
+            for i in queries:
+                if title in queries:
+                    article = util.get_entry(title)
+                    article_html = markdowner.convert(article)
+                    context = {
+                        'article': article_html,
+                        'title': title,
+                        'form': form
+                    }
+                    return render(request, 'encyclopedia/result.html', context)
+                if title.lower()in i.lower():
+                    query.append(i)
+                    context = {
+                        'query': query,
+                        'form' : Search() 
+                    }
+            return render(request, "encyclopedia/query.html", context)
+        else:
+            return render(request, "encyclopedia/index.html", {"form": form})
+    else:
+        return render(request, "encyclopedia/index.html", {
+            "entries": util.list_entries(), "form":Search()
+        })
+
+  
 
